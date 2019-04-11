d1be00756674e6a551c391ecd34351579126de01
Ronny Roeller <ronny@collaborne.com>
2019-04-11 14:32:14 +0200

    Literals must be declared as unsafeCSS

--- src/Literals/index.ts
+++ src/Literals/index.ts
@@ -1,46 +1,46 @@
-import {css} from 'lit-element';
+import {unsafeCSS} from 'lit-element';
 
-export const displayFlex = css`
+export const displayFlex = unsafeCSS`
   display: -ms-flexbox;
   display: -webkit-flex;
   display: flex;
 `;
 
-export const borderBox = css`
+export const borderBox = unsafeCSS`
   box-sizing: border-box;
 `;
 
-export const displayInlineFlex = css`
+export const displayInlineFlex = unsafeCSS`
   display: -ms-inline-flexbox;
   display: -webkit-inline-flex;
   display: inline-flex;
 `;
 
-export const horizontal = css`
+export const horizontal = unsafeCSS`
   -ms-flex-direction: row;
   -webkit-flex-direction: row;
   flex-direction: row;
 `;
 
-export const vertical = css`
+export const vertical = unsafeCSS`
   -ms-flex-direction: column;
   -webkit-flex-direction: column;
   flex-direction: column;
 `;
 
-export const wrap = css`
+export const wrap = unsafeCSS`
   -ms-flex-wrap: wrap;
   -webkit-flex-wrap: wrap;
   flex-wrap: wrap;
 `;
 
-export const noWrap = css`
+export const noWrap = unsafeCSS`
   -ms-flex-wrap: nowrap;
   -webkit-flex-wrap: nowrap;
   flex-wrap: nowrap;
 `;
 
-export const flexFactor = css`
+export const flexFactor = unsafeCSS`
   -ms-flex: 1 1 0.000000001px;
   -webkit-flex: 1;
   flex: 1;
@@ -48,119 +48,119 @@ export const flexFactor = css`
   flex-basis: 0.000000001px;
 `;
 
-export const flexFactorAuto = css`
+export const flexFactorAuto = unsafeCSS`
   -ms-flex: 1 1 auto;
   -webkit-flex: 1 1 auto;
   flex: 1 1 auto;
 `;
 
-export const flexFactorNone = css`
+export const flexFactorNone = unsafeCSS`
   -ms-flex: 1 1 auto;
   -webkit-flex: 1 1 auto;
   flex: 1 1 auto;
 `;
 
-export const displayNone = css`
+export const displayNone = unsafeCSS`
   display: none !important;
 `;
 
-export const flex2 = css`
+export const flex2 = unsafeCSS`
   -ms-flex: 2;
   -webkit-flex: 2;
   flex: 2;
 `;
 
-export const flex3 = css`
+export const flex3 = unsafeCSS`
   -ms-flex: 3;
   -webkit-flex: 3;
   flex: 3;
 `;
 
-export const flex4 = css`
+export const flex4 = unsafeCSS`
   -ms-flex: 4;
   -webkit-flex: 4;
   flex: 4;
 `;
 
-export const flex5 = css`
+export const flex5 = unsafeCSS`
   -ms-flex: 5;
   -webkit-flex: 5;
   flex: 5;
 `;
 
-export const flex6 = css`
+export const flex6 = unsafeCSS`
   -ms-flex: 6;
   -webkit-flex: 6;
   flex: 6;
 `;
 
-export const flex7 = css`
+export const flex7 = unsafeCSS`
   -ms-flex: 7;
   -webkit-flex: 7;
   flex: 7;
 `;
 
-export const flex8 = css`
+export const flex8 = unsafeCSS`
   -ms-flex: 8;
   -webkit-flex: 8;
   flex: 8;
 `;
 
-export const flex9 = css`
+export const flex9 = unsafeCSS`
   -ms-flex: 9;
   -webkit-flex: 9;
   flex: 9;
 `;
 
-export const flex10 = css`
+export const flex10 = unsafeCSS`
   -ms-flex: 10;
   -webkit-flex: 10;
   flex: 10;
 `;
 
-export const flex11 = css`
+export const flex11 = unsafeCSS`
   -ms-flex: 11;
   -webkit-flex: 11;
   flex: 11;
 `;
 
-export const flex12 = css`
+export const flex12 = unsafeCSS`
   -ms-flex: 12;
   -webkit-flex: 12;
   flex: 12;
 `;
 
-export const horizontalReverse = css`
+export const horizontalReverse = unsafeCSS`
   -ms-flex-direction: row-reverse;
   -webkit-flex-direction: row-reverse;
   flex-direction: row-reverse;
 `;
 
-export const verticalReverse = css`
+export const verticalReverse = unsafeCSS`
   -ms-flex-direction: column-reverse;
   -webkit-flex-direction: column-reverse;
   flex-direction: column-reverse;
 `;
 
-export const wrapReverse = css`
+export const wrapReverse = unsafeCSS`
   -ms-flex-wrap: wrap-reverse;
   -webkit-flex-wrap: wrap-reverse;
   flex-wrap: wrap-reverse;
 `;
 
-export const displayBlock = css`
+export const displayBlock = unsafeCSS`
   display: block;
 `;
 
-export const invisible = css`
+export const invisible = unsafeCSS`
   visibility: hidden !important;
 `;
 
-export const relative = css`
+export const relative = unsafeCSS`
   position: relative;
 `;
 
-export const fit = css`
+export const fit = unsafeCSS`
   position: absolute;
   top: 0;
   right: 0;
@@ -168,152 +168,152 @@ export const fit = css`
   left: 0;
 `;
 
-export const scroll = css`
+export const scroll = unsafeCSS`
   -webkit-overflow-scrolling: touch;
   overflow: auto;
 `;
 
-export const fixed = css`
+export const fixed = unsafeCSS`
   position: fixed;
 `;
 
-export const fixedTop = css`
+export const fixedTop = unsafeCSS`
   top: 0;
   left: 0;
   right: 0;
 `;
 
-export const fixedRight = css`
+export const fixedRight = unsafeCSS`
   top: 0;
   right: 0;
   bottom: 0;
 `;
 
-export const fixedLeft = css`
+export const fixedLeft = unsafeCSS`
   top: 0;
   bottom: 0;
   left: 0;
 `;
 
-export const fixedBottom = css`
+export const fixedBottom = unsafeCSS`
   right: 0;
   bottom: 0;
   left: 0;
 `;
 
-export const startAligned = css`
+export const startAligned = unsafeCSS`
   -ms-flex-align: start;
   -webkit-align-items: flex-start;
   align-items: flex-start;
 `;
 
-export const centerAligned = css`
+export const centerAligned = unsafeCSS`
   -ms-flex-align: center;
   -webkit-align-items: center;
   align-items: center;
 `;
 
-export const endAligned = css`
+export const endAligned = unsafeCSS`
   -ms-flex-align: end;
   -webkit-align-items: flex-end;
   align-items: flex-end;
 `;
 
-export const baseline = css`
+export const baseline = unsafeCSS`
   -ms-flex-align: baseline;
   -webkit-align-items: baseline;
   align-items: baseline;
 `;
 
-export const startJustified = css`
+export const startJustified = unsafeCSS`
   -ms-flex-pack: start;
   -webkit-justify-content: flex-start;
   justify-content: flex-start;
 `;
 
-export const centerJustified = css`
+export const centerJustified = unsafeCSS`
   -ms-flex-pack: center;
   -webkit-justify-content: center;
   justify-content: center;
 `;
 
-export const endJustified = css`
+export const endJustified = unsafeCSS`
   -ms-flex-pack: end;
   -webkit-justify-content: flex-end;
   justify-content: flex-end;
 `;
 
-export const aroundJustified = css`
+export const aroundJustified = unsafeCSS`
   -ms-flex-pack: distribute;
   -webkit-justify-content: space-around;
   justify-content: space-around;
 `;
 
-export const justified = css`
+export const justified = unsafeCSS`
   -ms-flex-pack: justify;
   -webkit-justify-content: space-between;
   justify-content: space-between;
 `;
 
-export const selfStart = css`
+export const selfStart = unsafeCSS`
   -ms-align-self: flex-start;
   -webkit-align-self: flex-start;
   align-self: flex-start;
 `;
 
-export const selfCenter = css`
+export const selfCenter = unsafeCSS`
   -ms-align-self: center;
   -webkit-align-self: center;
   align-self: center;
 `;
 
-export const selfEnd = css`
+export const selfEnd = unsafeCSS`
   -ms-align-self: flex-end;
   -webkit-align-self: flex-end;
   align-self: flex-end;
 `;
 
-export const selfStretch = css`
+export const selfStretch = unsafeCSS`
   -ms-align-self: stretch;
   -webkit-align-self: stretch;
   align-self: stretch;
 `;
 
-export const selfBaseline = css`
+export const selfBaseline = unsafeCSS`
   -ms-align-self: baseline;
   -webkit-align-self: baseline;
   align-self: baseline;
 `;
 
-export const startAlignedContent = css`
+export const startAlignedContent = unsafeCSS`
   -ms-flex-line-pack: start; /* IE10 */
   -ms-align-content: flex-start;
   -webkit-align-content: flex-start;
   align-content: flex-start;
 `;
 
-export const endAlignedContent = css`
+export const endAlignedContent = unsafeCSS`
   -ms-flex-line-pack: end; /* IE10 */
   -ms-align-content: flex-end;
   -webkit-align-content: flex-end;
   align-content: flex-end;
 `;
 
-export const centerAlignedContent = css`
+export const centerAlignedContent = unsafeCSS`
   -ms-flex-line-pack: center; /* IE10 */
   -ms-align-content: center;
   -webkit-align-content: center;
   align-content: center;
 `;
 
-export const beteweenAlignedContent = css`
+export const beteweenAlignedContent = unsafeCSS`
   -ms-flex-line-pack: justify; /* IE10 */
   -ms-align-content: space-between;
   -webkit-align-content: space-between;
   align-content: space-between;
 `;
 
-export const aroundAlignedContent = css`
+export const aroundAlignedContent = unsafeCSS`
   -ms-flex-line-pack: distribute; /* IE10 */
   -ms-align-content: space-around;
   -webkit-align-content: space-around;
